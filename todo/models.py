82bc18034ea0558d6c17cd1f936064747fd838b5
jiyeonCoder <jiyeon.choi218@gmail.com>
2023-09-04 11:03:26 +0900

    add pillow dependency

---
+++ todo/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Todo(models.Model):
+    user = models.ForeignKey('user.User', on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_done = models.BooleanField(default=False)
+    image = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return self.content
---
+++ todo/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.http import HttpResponse
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.index),
+    path('create/', views.create),
+    path('<int:todo_id>/', views.read),
+    path('delete/<int:todo_id>/', views.delete),
+    path('update/<int:todo_id>/', views.update),
+]
